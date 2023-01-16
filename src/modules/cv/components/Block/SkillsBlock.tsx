abb0cc269b6091cfee6328e4654cca4e13fb68b6
maki <marketa@vlach.io>
2023-01-17 00:24:33 +0100

    Add progressbar component

--- src/modules/cv/components/Block/SkillsBlock.tsx
+++ src/modules/cv/components/Block/SkillsBlock.tsx
@@ -1,9 +1,12 @@
 import {SectionBlock, SectionBlockProps} from "@/modules/cv/components/Block/SectionBlock";
 import { GiSkills } from 'react-icons/gi'
+import ProgressBar from "@/modules/cv/components/ProgressBar/ProgressBar";
 
 type SkillsBlockProps = {
 }
 
 export const SkillsBlock: React.FC<SkillsBlockProps> = ({...props}) => {
-    return <SectionBlock title='Skills' icon={GiSkills} />
+    return <SectionBlock title='Skills' icon={GiSkills}>
+        <ProgressBar value={80}/>
+    </SectionBlock>
 }
--- src/modules/cv/const/colors.ts
+++ src/modules/cv/const/colors.ts
@@ -1,3 +1,4 @@
 export const PRIMARY_COLOR = '#e3dbd1'
 export const SECONDARY_COLOR = '#ded1c1'
 export const BACKGROUND_COLOR = '#f5f5f5'
+export const PROGRESSBAR_COLOR = '#2e2e2e'
