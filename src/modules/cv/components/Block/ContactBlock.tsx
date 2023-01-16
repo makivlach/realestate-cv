3d57c8fcd01488ce6b3fbe470924b2af0338a2b0
maki <marketa@vlach.io>
2023-01-16 23:27:35 +0100

    Change contact icons

--- src/modules/cv/components/Block/ContactBlock.tsx
+++ src/modules/cv/components/Block/ContactBlock.tsx
@@ -1,4 +1,4 @@
-import {FiMapPin, FiPhone, FiMail}  from 'react-icons/fi'
+import {IoIosPin, IoIosCall, IoIosMail}  from 'react-icons/io'
 import React from "react";
 import {Heading} from "@/modules/cv/components/Heading";
 import {Text} from "@/modules/cv/components/Text";
@@ -14,9 +14,9 @@ export const ContactBlock: React.FC<ContactBlockProps> = (props) => {
         <div>
             <Heading level='H2'>Contact</Heading>
             <div className='mt-2 flex flex-col gap-1'>
-                {props.address ? <Text level='span' className='flex gap-2'><FiMapPin className='mt-1' /> {props.address}</Text> : null}
-                {props.phone ? <Text level='span' className='flex items-center gap-2'><FiPhone /> <a href='mob:+420604537549'>{props.phone}</a></Text> : null}
-                {props.email ? <Text level='span' className='flex items-center gap-2'><FiMail /> <a href='mailto:marketa@vlach.io'>{props.email}</a></Text> : null}
+                {props.address ? <Text level='span' className='flex gap-2'><IoIosPin size={24} className='mt-1' /> {props.address}</Text> : null}
+                {props.phone ? <Text level='span' className='flex items-center gap-2'><IoIosCall size={24} /> <a href='mob:+420604537549'>{props.phone}</a></Text> : null}
+                {props.email ? <Text level='span' className='flex items-center gap-2'><IoIosMail size={24} /> <a href='mailto:marketa@vlach.io'>{props.email}</a></Text> : null}
             </div>
         </div>
     )
