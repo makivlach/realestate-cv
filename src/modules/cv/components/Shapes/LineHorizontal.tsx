592f5005d85c76bccd9d5d9cd90c4d3c64cd516b
maki <marketa@vlach.io>
2023-01-17 01:11:47 +0100

    Add LineHorizontal into Shapes

---
+++ src/modules/cv/components/Shapes/LineHorizontal.tsx
@@ -0,0 +1,1 @@
+export const LineHorizontal: React.FC<{style?: React.CSSProperties, className?: string}> = ({className, style, ...props}) => <div {...props} style={{...style, height: 2}} className={'w-full bg-gray-600 ' + className} />
